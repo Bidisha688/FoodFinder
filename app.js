28efb56270ca3b4cdfea56d79f9471352f700609
ArponRoy7 <arponkolkata7@gmail.com>
2025-07-19 17:25:43 +0530

    builed enviroment

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-/*<div id="parent">
-  <div id="child">
-    <h1>I'm h1 tag</h1>
-  </div>
-</div>
-*/
+import React from "react"
+import ReactDOM from "react-dom/client";
+
         const parent = React.createElement("div",{id:"parent"},
             [React.createElement("div",{id:"child 1 "},
             [React.createElement("h1",{},"i am  a h1 tag"),
